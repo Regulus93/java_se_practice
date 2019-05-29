eb2cd5cf5823914a2cad2b67f644b9ecf98db97a
Daniel Bicsak <bicsak.daniel@gmail.com>
2019-05-29 15:07:36 +0200

    [Chapter-03] Chapter done

---
+++ src/headfirstjava/chapter-03/ChapterThree.java
@@ -0,0 +1,20 @@
+public class ChapterThree {
+	
+	public static void main(String[] args) {
+		//Example for possible lossy conversion from int to byte
+		// int x = 24;
+		// byte newX = x;
+		
+		int[] integerArray = new int[3];
+		
+		short s = 2;
+		byte b = 4;
+		int i = 6;
+		
+		integerArray[0] = s;
+		integerArray[1] = b;
+		integerArray[2] = i;
+		
+	}
+	
+}
